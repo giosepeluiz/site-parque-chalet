8531fcedd313b884364d1a9f9b9f51e8f4eafa5f
Giosepe Luiz <giosepe_luiz3@live.com>
2022-02-07 15:00:51 -0300

    Página de login uCondo

--- src/pages/entrar/index.js
+++ src/pages/entrar/index.js
@@ -1,7 +1,7 @@
 import { NextSeo } from "next-seo";
-// import Image from "next/image";
+import Image from "next/image";
 import { useEffect } from "react";
-// import logo from "../../../public/images/avatar-logo-chalet.jpg";
+import logo from "../../../public/images/avatar-logo-chalet.jpg";
 import styles from "../../styles/Home.module.scss";
 
 export default function Home() {
