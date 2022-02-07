c85219e15038bc25a4cede47120b2f9e39141a7e
Giosepe Luiz <giosepe_luiz3@live.com>
2022-02-07 19:32:53 -0300

    uCondo link

---
+++ src/pages/ucondo/android/index.js
@@ -0,0 +1,51 @@
+import { NextSeo } from "next-seo";
+import Image from "next/image";
+import { useEffect } from "react";
+import logo from "../../../../public/images/avatar-logo-chalet.jpg";
+import styles from "../../../styles/Home.module.scss";
+
+export default function Home() {
+  useEffect(() => {
+    
+    window.location.href = "https://play.google.com/store/apps/details?id=br.com.ucondo.android&hl=pt&gl=BR";
+
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {/* SEO com personalização - Documentação: https://www.npmjs.com/package/next-seo */}
+      <NextSeo
+        title="Sistema uCondo - Condomínio Parque Chalet"
+        description="Sistema uCondo - Condomínio Parque Chalet"
+        openGraph={{
+          type: "website",
+          url: "https://parquechalet.com.br",
+          title: "Sistema uCondo - Condomínio Parque Chalet",
+          description: "Sistema uCondo - Condomínio Parque Chalet",
+          images: [
+            {
+              url: "https://parquechalet.com.br/images/og-banner.jpg",
+              width: 800,
+              height: 420,
+              alt: "Sistema uCondo - Condomínio Parque Chalet",
+            },
+          ],
+          site_name: "Sistema uCondo - Condomínio Parque Chalet",
+        }}
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "icons/favicon.ico",
+          },
+        ]}
+      />
+
+      <main className={styles.main}>
+        <Image src={logo} width="300" height="300" alt="Condomínio Parque Chalet" />
+        <h1 className={styles.redirect}>Redirecionando para Google Play...</h1>
+      </main>
+
+      {/* <footer className={styles.footer} /> */}
+    </div>
+  );
+}
