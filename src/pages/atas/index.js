9f82ee2ae9aea9d8478b7f269f9c8b670198fe0c
Giosepe Luiz <giosepe_luiz3@live.com>
2022-02-08 23:09:17 -0300

    Atualização sistema uCondo

--- src/pages/atas/index.js
+++ src/pages/atas/index.js
@@ -1,29 +1,36 @@
 import { NextSeo } from "next-seo";
-// import Image from "next/image";
-// import logo from "../../../public/images/avatar-logo-chalet.jpg";
+import Image from "next/image";
+import { useEffect } from "react";
+import logo from "../../../public/images/avatar-logo-chalet.jpg";
 import styles from "../../styles/Home.module.scss";
 
 export default function Home() {
+  useEffect(() => {
+        
+    window.location.href = "https://web.ucondo.com.br/login.aspx";
+
+  }, []);
+
   return (
     <div className={styles.container}>
       {/* SEO com personalização - Documentação: https://www.npmjs.com/package/next-seo */}
       <NextSeo
-        title="Atas de Assembleias - Condomínio Parque Chalet"
-        description="Atas de Assembleias - Condomínio Parque Chalet"
+        title="Sistema uCondo - Condomínio Parque Chalet"
+        description="Sistema uCondo - Condomínio Parque Chalet"
         openGraph={{
           type: "website",
           url: "https://parquechalet.com.br",
-          title: "Atas de Assembleias - Condomínio Parque Chalet",
-          description: "Atas de Assembleias - Condomínio Parque Chalet",
+          title: "Sistema uCondo - Condomínio Parque Chalet",
+          description: "Sistema uCondo - Condomínio Parque Chalet",
           images: [
             {
               url: "https://parquechalet.com.br/images/og-banner.jpg",
               width: 800,
               height: 420,
-              alt: "Atas de Assembleias - Condomínio Parque Chalet",
+              alt: "Sistema uCondo - Condomínio Parque Chalet",
             },
           ],
-          site_name: "Atas de Assembleias - Condomínio Parque Chalet",
+          site_name: "Sistema uCondo - Condomínio Parque Chalet",
         }}
         additionalLinkTags={[
           {
@@ -33,23 +40,10 @@ export default function Home() {
         ]}
       />
 
-      <iframe
-        src="https://drive.google.com/a/parquechalet.com.br/embeddedfolderview?id=1-cQrmUbzKrsPdzLVCD7pVZGgnzXltvOz#grid"
-        title="Atas de Assembleias - Condomínio Parque Chalet"
-        style={{
-          border: "0px",
-          padding: "0px",
-          margin: "0px",
-          width: "100vw",
-          height: "100vh",
-        }}
-      />
-
-      {/* Conteúdo principal */}
-      {/* <main className={styles.main}>
+      <main className={styles.main}>
         <Image src={logo} width="300" height="300" alt="Condomínio Parque Chalet" />
-        <h1 className={styles.redirect}>Redirecionando...</h1>
-      </main> */}
+        <h1 className={styles.redirect}>Redirecionando para o sistema uCondo...</h1>
+      </main>
 
       {/* <footer className={styles.footer} /> */}
     </div>
---
+++ src/pages/balancetes/OLD_index.js
@@ -0,0 +1,57 @@
+import { NextSeo } from "next-seo";
+// import Image from "next/image";
+// import logo from "../../../public/images/avatar-logo-chalet.jpg";
+import styles from "../../styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      {/* SEO com personalização - Documentação: https://www.npmjs.com/package/next-seo */}
+      <NextSeo
+        title="Balancetes - Condomínio Parque Chalet"
+        description="Balancetes - Condomínio Parque Chalet"
+        openGraph={{
+          type: "website",
+          url: "https://parquechalet.com.br",
+          title: "Balancetes - Condomínio Parque Chalet",
+          description: "Balancetes - Condomínio Parque Chalet",
+          images: [
+            {
+              url: "https://parquechalet.com.br/images/og-banner.jpg",
+              width: 800,
+              height: 420,
+              alt: "Balancetes - Condomínio Parque Chalet",
+            },
+          ],
+          site_name: "Balancetes - Condomínio Parque Chalet",
+        }}
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "icons/favicon.ico",
+          },
+        ]}
+      />
+
+      <iframe
+        src="https://drive.google.com/a/parquechalet.com.br/embeddedfolderview?id=12WqhVI3R5CL5GxXu8hZPY4_KyYaOVoEu#grid"
+        title="Balancetes - Condomínio Parque Chalet"
+        style={{
+          border: "0px",
+          padding: "0px",
+          margin: "0px",
+          width: "100vw",
+          height: "100vh",
+        }}
+      />
+
+      {/* Conteúdo principal */}
+      {/* <main className={styles.main}>
+        <Image src={logo} width="300" height="300" alt="Condomínio Parque Chalet" />
+        <h1 className={styles.redirect}>Redirecionando...</h1>
+      </main> */}
+
+      {/* <footer className={styles.footer} /> */}
+    </div>
+  );
+}
